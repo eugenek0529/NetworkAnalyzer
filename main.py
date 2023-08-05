3d99f152b4e28d46495e51bc30de1018eb888090
Eugene Kim <eugenek0529@gmail.com>
2023-08-04 19:25:22 -0700

    Intro memo created

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+    Program: Network Analyer
+    Programmer: Eugene Kim
+    About:
+        Goal of this program is to analyze the Network
+        Packets.
+
+        * Choose either to capture real time data capture
+          or existing pcap file
+        * Study structure of pcap file
+            - What is it contain
+            - How is it organized, and be accessed
+        * Set the sign of alert
+        * What can I do with the alert?
+            - What is machine learning do? 
+
+
+"""
